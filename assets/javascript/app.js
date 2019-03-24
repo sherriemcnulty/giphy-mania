be9d90303c8b1d92d84e9c809a5f572ca26fac49
Sherrie McNulty <sherriemcnulty@gmail.com>
2019-03-24 02:16:10 -0400

    I specified G rating to avoid offensive images, but I still have to display the ratings with the images.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
      'use strict';
 
-     // ----- Global Variables -----//
+     // ----- VARIABLES -----//
      var topics = [
           'Tom And Jerry',
           'Lady And The Tramp',
@@ -11,8 +11,61 @@ $(document).ready(function () {
           'Betty Boop'
      ];
 
-     // -----  Functions -----//
-     function renderButtons() {
+     // create seed buttons
+     createButtons();
+
+     // ----- EVENTS -----//
+
+     // submit button click adds the new topic to the topics array
+     $('#submit-btn').on('click', function (event) {
+
+          event.preventDefault();
+
+          var topic = $('#search-input').val().trim();
+
+          if (topic == '') {
+               alert('Oops! You forgot to enter a title.');
+               return;
+          }
+
+          topics.push(topic);
+          createButtons();
+     }); // submit button click
+
+     // topic button click displays images
+     $(document).on('click', '.topic-btn', function () {
+
+          var title = $(this).attr('data-name');
+
+          displayImages(title);
+
+     }); // topic button click
+
+     // image click toggles between still and animated states
+     $(document).on('click', '.gif', function () {
+
+          event.preventDefault();
+
+          var state = $(this).attr("data-state");
+
+          if (state === 'still') {
+
+               // set src to animated gif & update data-state 
+               $(this).attr('src', $(this).attr('data-animate'));
+               $(this).attr('data-state', 'animate');
+
+          } else {
+
+               // set src to still gif & update data-state
+               $(this).attr('src', $(this).attr('data-still'));
+               $(this).attr('data-state', 'still');
+          }
+     }); // image click
+
+     // -----  FUNCTIONS -----//
+
+     // create topic buttons
+     function createButtons() {
 
           // delete to avoid duplicates
           $('#topic-buttons').empty();
@@ -26,35 +79,16 @@ $(document).ready(function () {
                btn.text(topics[i]);
                $('#topic-buttons').append(btn);
           }
-     }
+     } // createButtons()
 
-     function displayGIF(json) {
-
-          // delete old gifs
-          $('#topic-view').empty();
-
-          // display gifs
-          for (var i = 0; i < json.data.length; i++) {
-
-               var stillGIF = json.data[i].images.original_still.url;
-               var animatedGIF = json.data[i].images.original.url;
-               var alt = json.data[i].title;
-
-               console.log('# images: ' + json.data.length);
-               var image = '<img class="gif" data-state="still" src="' + stillGIF + '" data-animate="' + animatedGIF + '" data-still="' + stillGIF + '" alt="' + alt + '">';
-
-               $('#topic-view').append(image);
-          }
-     }
-
-     function addGIF(title) {
+     // extract and display GIF images
+     function displayImages(title) {
 
           var searchString = title.split(' ').join('-');
           var baseURL = 'https://api.giphy.com/v1/gifs/search?q=';
           var apiKey = '&api_key=9EzP8xJDoAwo0A6yOFyhFlCAlOJ59zNw';
           var limit = '&limit=10';
           var rating = '&rating=g';
-
           var queryURL = baseURL + searchString + apiKey + rating + limit;
 
           try {
@@ -64,60 +98,26 @@ $(document).ready(function () {
                     method: 'GET'
                }).then(function (response) {
 
-                    displayGIF(response);
+                    var data = response.data;
+
+                    // delete old gifs
+                    // $('#topic-view').empty();
+
+                    // display images
+                    for (var i = 0; i < data.length; i++) {
+
+                         var stillGIF = data[i].images.original_still.url;
+                         var animatedGIF = data[i].images.original.url;
+                         var alt = data[i].title;
+
+                         var image = `<img class="gif" data-state="still" src="${stillGIF}" data-animate="${animatedGIF}" data-still="${stillGIF} alt="${alt}">`;
+
+                         $('#topic-view').prepend(image);
+                    }
                });
           } catch (e) {
 
                console.log('Something went wrong with AJAX call.');
           }
      }
-
-     // ----- Click Events ----- //
-     $('#search-btn').on('click', function (event) {
-
-          event.preventDefault();
-
-          var topic = $('#search-input').val().trim();
-
-          if (topic == '') {
-               alert('Oops! You forgot to enter a title.');
-               return;
-          }
-
-          topics.push(topic);
-          renderButtons();
-     });
-
-     $(document).on('click', '.topic-btn', function () {
-
-          var title = $(this).attr('data-name');
-
-          addGIF(title);
-
-     });
-
-     $(document).on('click', '.gif', function () {
-
-          console.log("inside click function");
-
-          event.preventDefault();
-
-          var state = $(this).attr("data-state");
-
-          if (state === 'still') {
-
-               // set src to animated gif & update data-state        
-               $(this).attr('src', $(this).attr('data-animate'));
-               $(this).attr('data-state', 'animate');
-
-          } else {
-
-               // set src to still gif & update data-state
-               $(this).attr('src', $(this).attr('data-still'));
-               $(this).attr('data-state', 'still');
-          }
-     });
-
-     // -----  Render seed buttons -----//
-     renderButtons();
-});
+}); // displayImages()
