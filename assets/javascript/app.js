2e8c9bb6626e227321818f0f349ccfbea5eee24f
Sherrie McNulty <sherriemcnulty@gmail.com>
2019-03-31 16:26:59 -0400

    Improved appearance.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,146 +1,147 @@
-$(document).ready(function () {
+'use strict';
 
-     'use strict';
+// ----- VARIABLES -----//
+var topics = [
+     'Daffy Duck',
+     'Donald Duck',
+     'Betty Boop'
+];
 
-     // ----- VARIABLES -----//
-     var topics = [
-          'Daffy Duck',
-          'Donald Duck',
-          'Betty Boop'
-     ];
+// create seed buttons
+createButtons();
 
-     // create seed buttons
+// ----- EVENTS -----//
+
+// submit button click adds the new topic to the topics array
+$('#submit-btn').on('click', function (event) {
+
+     event.preventDefault();
+
+     var topic = $('#search-input').val().trim();
+
+     if (topic == '') {
+          alert('Oops! You forgot to enter a title.');
+          return;
+     }
+
+     topics.push(topic);
      createButtons();
+}); // submit button click
 
-     // ----- EVENTS -----//
+// topic button click displays images
+$(document).on('click', '.topic-btn', function () {
 
-     // submit button click adds the new topic to the topics array
-     $('#submit-btn').on('click', function (event) {
+     var title = $(this).attr('data-name');
 
-          event.preventDefault();
+     displayImages(title);
 
-          var topic = $('#search-input').val().trim();
+}); // topic button click
 
-          if (topic == '') {
-               alert('Oops! You forgot to enter a title.');
-               return;
-          }
+$(document).on('click', '.clear-btn', function () {
 
-          topics.push(topic);
-          createButtons();
-     }); // submit button click
+     // clear images from topic-view
 
-     // topic button click displays images
-     $(document).on('click', '.topic-btn', function () {
+     $('#topic-view').empty();
+});
 
-          var title = $(this).attr('data-name');
+// image click toggles between still and animated states
+$(document).on('click', '.gif', function () {
 
-          displayImages(title);
+     event.preventDefault();
 
-     }); // topic button click
+     var state = $(this).attr("data-state");
 
-     // image click toggles between still and animated states
-     $(document).on('click', '.gif', function () {
+     if (state === 'still') {
 
-          event.preventDefault();
+          // set src to animated gif & update data-state 
+          $(this).attr('src', $(this).attr('data-animate'));
+          $(this).attr('data-state', 'animate');
 
-          var state = $(this).attr("data-state");
+     } else {
 
-          if (state === 'still') {
+          // set src to still gif & update data-state
+          $(this).attr('src', $(this).attr('data-still'));
+          $(this).attr('data-state', 'still');
+     }
+}); // image click
 
-               // set src to animated gif & update data-state 
-               $(this).attr('src', $(this).attr('data-animate'));
-               $(this).attr('data-state', 'animate');
+// -----  FUNCTIONS -----//
 
-          } else {
+// create topic buttons
+function createButtons() {
 
-               // set src to still gif & update data-state
-               $(this).attr('src', $(this).attr('data-still'));
-               $(this).attr('data-state', 'still');
-          }
-     }); // image click
-
-     // -----  FUNCTIONS -----//
+     // delete to avoid duplicates
+     $('#topic-buttons').empty();
 
      // create topic buttons
-     function createButtons() {
+     for (var i = 0; i < topics.length; i++) {
 
-          // delete to avoid duplicates
-          $('#topic-buttons').empty();
+          var btn = $('<button>');
+          btn.addClass('topic-btn m-1');
+          btn.attr('data-name', topics[i]);
+          btn.text(topics[i]);
+          $('#topic-buttons').append(btn);
+     }
+} // createButtons()
 
-          // create topic buttons
-          for (var i = 0; i < topics.length; i++) {
+// extract and display GIF images
+function displayImages(title) {
 
-               var btn = $('<button>');
-               btn.addClass('topic-btn m-1');
-               btn.attr('data-name', topics[i]);
-               btn.text(topics[i]);
-               $('#topic-buttons').append(btn);
-          }
-     } // createButtons()
+     var searchString = title.split(' ').join('-');
+     var baseURL = 'https://api.giphy.com/v1/gifs/search?q=';
+     var apiKey = '&api_key=9EzP8xJDoAwo0A6yOFyhFlCAlOJ59zNw';
+     var limit = '&limit=12';
+     var rating = '&rating=g';
+     var queryURL = baseURL + searchString + apiKey + rating + limit;
 
-     // extract and display GIF images
-     function displayImages(title) {
+     try {
+          $.ajax({
 
-          var searchString = title.split(' ').join('-');
-          var baseURL = 'https://api.giphy.com/v1/gifs/search?q=';
-          var apiKey = '&api_key=9EzP8xJDoAwo0A6yOFyhFlCAlOJ59zNw';
-          var limit = '&limit=10';
-          var rating = '&rating=g';
-          var queryURL = baseURL + searchString + apiKey + rating + limit;
+               url: queryURL,
+               method: 'GET'
+          }).then(function (response) {
 
-          try {
-               $.ajax({
+               var data = response.data;
 
-                    url: queryURL,
-                    method: 'GET'
-               }).then(function (response) {
+               // we need to declare $row outside the loop
+               // to avoid creating a new one for each image
+               var $row;
 
-                    // clear old gifs so only new ones are displayed
-                    // $('#topic-view').empty();
+               for (var i = 0; i < data.length; i++) {
 
-                    var data = response.data;
+                    // grab urls and ratings
+                    var stillGif = data[i].images.original_still.url;
+                    var animatedGif = data[i].images.original.url;
+                    var alt = data[i].title;
+                    var rating = data[i].rating;
 
-                    // we need to declare $row outside the loop
-                    // to avoid creating a new one for each image
-                    var $row;
+                    // create elements and append to 
+                    var $img = `<img class="gif overflow-hidden" data-state="still" src="${stillGif}" data-animate="${animatedGif}" data-still="${stillGif}" alt="${alt}"><br>`;
 
-                    for (var i = 0; i < data.length; i++) {
+                    var $p = $("<p>");
+                    $p.text(`Rating: ${data[i].rating}`);
+                    $p.attr("id", `p${i}`);
 
-                         // grab urls and ratings
-                         var stillGif = data[i].images.original_still.url;
-                         var animatedGif = data[i].images.original.url;
-                         var alt = data[i].title;
-                         var rating = data[i].rating;
+                    var $col = $("<div>");
+                    $col.addClass("col-md-2");
+                    $col.attr("id", `col${i}`);
 
-                         // create elements and append to 
-                         var $img = `<img class="gif overflow-hidden" data-state="still" src="${stillGif}" data-animate="${animatedGif}" data-still="${stillGif}" alt="${alt}"><br>`;
+                    // append image and rating
+                    if (!(i % 6)) {
+                         $row = $("<div>");
+                         $row.addClass("row");
+                         $row.addClass("my-3");
+                    }
+                    $row.append($col);
+                    $col.append($img);
+                    $col.append($p);
+                    $("#topic-view").prepend($row);
 
-                         var $p = $("<p>");
-                         $p.text(`Rating: ${data[i].rating}`);
-                         $p.attr("id", `p${i}`);
+               } // for
+          }); // AJAX call
 
-                         var $col = $("<div>");
-                         $col.addClass("col-md-3");
-                         $col.attr("id", `col${i}`);
+     } catch (e) {
 
-                         // append image and rating
-                         if (!(i % 4)) {
-                              $row = $("<div>");
-                              $row.addClass("row");
-                              $row.addClass("my-3");
-                         }
-                         $row.append($col);
-                         $col.append($img);
-                         $col.append($p);
-                         $("#topic-view").prepend($row);
-
-                    } // for
-               }); // AJAX call
-
-          } catch (e) {
-
-               console.log('Something went wrong with AJAX call.');
-          }
-     } // displayImages()
-}); // document.ready()
+          console.log('Something went wrong with AJAX call.');
+     }
+} // displayImages()
